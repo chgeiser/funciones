189924bbb6ae5f155afdd38744068de1b4e3ef9a
Christopher Geiser <christopher.geiser@gmail.com>
2025-10-19 21:25:19 -0300

    letras

---
+++ assets/js/letra.js
@@ -0,0 +1,30 @@
+let color = "";
+
+document.addEventListener("keydown", function (event) {
+    const tecla = event.key.toLowerCase(); // para manejar mayúsculas o minúsculas
+    const divKey = document.getElementById("key");
+
+  if (tecla === "a") {
+    color = 'pink';
+    divKey.style.backgroundColor = color;
+  } else if (tecla === "s") {
+    color = 'orange';
+    divKey.style.backgroundColor = color;
+  } else if (tecla === "d") {
+    color = 'lightblue';
+    divKey.style.backgroundColor = color;
+  } else if (tecla === "q") {
+    crearDiv("purple");
+  } else if (tecla === "w") {
+    crearDiv("gray");
+  } else if (tecla === "e") {
+    crearDiv("brown");
+  }
+});
+
+function crearDiv(colorNuevo) {
+  const nuevo = document.createElement("div");
+  nuevo.classList.add("nuevo-div");
+  nuevo.style.backgroundColor = colorNuevo;
+  document.body.appendChild(nuevo);
+}
